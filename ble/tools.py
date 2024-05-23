3638031bd6f554f145e58fbe04f0a2493b700098
Walkline Wang <walkline@163.com>
2024-05-23 16:14:01 +0800

    tools: 优化代码，增加新的函数
    增加了字节数据解码函数
    
    * decode_name：用于解码设备名称
    * decode_services：用于解码服务 UUID
    * decode_services_data：用于解码服务数据
    * __decode_field：以上函数的辅助函数
    
    Signed-off-by: Walkline Wang <walkline@163.com>

--- ble/tools.py
+++ ble/tools.py
@@ -2,8 +2,9 @@
 Copyright © 2024 Walkline Wang (https://walkline.wang)
 Gitee: https://gitee.com/walkline/micropython-new-ble-library
 """
-from .consts import *
-from struct import pack
+from micropython import const
+from struct import pack, unpack
+from bluetooth import UUID
 
 
 class ADType(object):
@@ -11,9 +12,11 @@ class ADType(object):
 	BIT16_SERVICE_UUID_COMPLETE = const(0x03)
 	BIT32_SERVICE_UUID_COMPLETE = const(0x05)
 	BIT128_SERVICE_UUID_COMPLETE = const(0x07)
+	COMPLETE_LOCAL_NAME = const(0x09)
+	TX_POWER_LEVEL = const(0x0A)
+	SERVICE_DATA = const(0x16) # Service Data - 16-bit UUID
 	APPEARANCE_ = const(0x19)
 	MANUFACTURER_SPECIFIC_DATA = const(0xFF)
-	COMPLETE_LOCAL_NAME = const(0x09)
 
 
 class BLETools(object):
@@ -55,6 +58,48 @@ class BLETools(object):
 
 		assert isinstance(addr, bytes) and len(addr) == 6, ValueError('mac address value error')
 		return ':'.join(['%02X' % byte for byte in addr])
+
+	@staticmethod
+	def decode_name(payload):
+		n = BLETools.__decode_field(payload, ADType.COMPLETE_LOCAL_NAME)
+		return str(n[0], "utf-8") if n else ""
+
+	@staticmethod
+	def decode_services(payload):
+		services = []
+
+		for u in BLETools.__decode_field(payload, ADType.BIT16_SERVICE_UUID_COMPLETE):
+			services.append(UUID(unpack("<h", u)[0]))
+		for u in BLETools.__decode_field(payload, ADType.BIT32_SERVICE_UUID_COMPLETE):
+			services.append(UUID(unpack("<d", u)[0]))
+		for u in BLETools.__decode_field(payload, ADType.BIT128_SERVICE_UUID_COMPLETE):
+			services.append(UUID(u))
+
+		return services
+
+	@staticmethod
+	def decode_services_data(payload):
+		services = []
+		data = []
+
+		for u in BLETools.__decode_field(payload, ADType.SERVICE_DATA):
+			services.append(UUID(u[:2]))
+			data.append(u[2:])
+
+		return services, data
+
+	@staticmethod
+	def __decode_field(payload, adv_type):
+		i = 0
+		result = []
+
+		while i + 1 < len(payload):
+			if payload[i + 1] == adv_type:
+				result.append(payload[i + 2 : i + payload[i] + 1])
+
+			i += 1 + payload[i]
+
+		return result
 
 	@staticmethod
 	def make_appearance(category:int, subcategory:int) -> int:
