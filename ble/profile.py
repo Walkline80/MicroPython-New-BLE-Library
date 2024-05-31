d5729f5f6c2587a868c7b99736dfb007db7d4d03
Walkline Wang <walkline@163.com>
2024-05-31 16:26:50 +0800

    profile: 增加函数说明文字
    Signed-off-by: Walkline Wang <walkline@163.com>

--- ble/profile.py
+++ ble/profile.py
@@ -10,6 +10,7 @@ class Profile(object):
 		self.__services = []
 
 	def add_services(self, *services):
+		'''添加服务'''
 		for service in services:
 			update = False
 
@@ -23,10 +24,13 @@ class Profile(object):
 				self.__services.append(service)
 
 	def get_services(self) -> list:
+		'''获取配置文件的服务、特征和描述符列表'''
 		return [service.get_service() for service in self.__services]
 
 	def get_services_uuid(self) -> list:
+		'''获取配置文件的服务、特征和描述符 UUID 列表'''
 		return [service.__uuid for service in self.__services]
+
 
 class Service(object):
 	def __dir__(self):
@@ -37,12 +41,15 @@ class Service(object):
 		self.__characteristics = []
 
 	def add_characteristics(self, *characteristics):
+		'''添加特征'''
 		for characteristic in characteristics:
 			self.__characteristics.append(characteristic)
 		return self
 
 	def get_service(self) -> list:
+		'''获取当前服务列表'''
 		return [self.__uuid, [char.get_characteristic() for char in self.__characteristics]]
+
 
 class Characteristic(object):
 	def __dir__(self):
@@ -54,15 +61,18 @@ class Characteristic(object):
 		self.__flags = flags
 
 	def add_descriptors(self, *descriptors):
+		'''添加描述符'''
 		for descriptor in descriptors:
 			self.__descriptors.append(descriptor)
 		return self
 
 	def get_characteristic(self) -> list:
+		'''获取当前特征列表'''
 		if self.__descriptors:
 			return [self.__uuid, self.__flags, [descriptor.get_descriptor() for descriptor in self.__descriptors]]
 		else:
 			return [self.__uuid, self.__flags]
+
 
 class Descriptor(object):
 	def __dir__(self):
@@ -73,4 +83,5 @@ class Descriptor(object):
 		self.__flags = flags
 
 	def get_descriptor(self) -> list:
+		'''获取当前描述符列表'''
 		return [self.__uuid, self.__flags]
