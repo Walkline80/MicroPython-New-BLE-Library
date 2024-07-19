56536e823a5282335086772dbb7df0b88e37ad8b
Walkline Wang <walkline@163.com>
2024-07-19 10:52:04 +0800

    uart: 优化代码
    * 更新 abconfig 文件
    * 修改 BLEUART 类相关函数名称，更符合串口类库实现的接口规范
    
    Signed-off-by: Walkline Wang <walkline@163.com>

--- devices/uart/bleuart.py
+++ devices/uart/bleuart.py
@@ -7,10 +7,6 @@ from ble import *
 from profiles.uart import UARTProfile
 
 
-def printf(msg, *args, **kwargs):
-	print(f'\033[1;37m[INFO]\033[0m {msg}', *args, **kwargs)
-
-
 class BLEUART(object):
 	'''主、从机数据交换'''
 	def __dir__(self):
@@ -19,14 +15,10 @@ class BLEUART(object):
 	def __init__(self, device_name: str = 'ble-uart', rx_received_cb: function = None):
 		self.__ble            = bluetooth.BLE()
 		self.__rx_received_cb = rx_received_cb
+		self.__rx_buffer      = bytearray()
 		self.__conn_handles   = set()
 
 		appearance = 384 # (0x006, 0x00)
-
-		self.__write    = self.__ble.gatts_write
-		self.__read     = self.__ble.gatts_read
-		self.__notify   = self.__ble.gatts_notify
-		self.__indicate = self.__ble.gatts_indicate
 
 		self.__ble.config(gap_name=device_name)
 		self.__ble.irq(self.__irq_callback)
@@ -61,16 +53,19 @@ class BLEUART(object):
 	def __register_services(self, services: list):
 		(
 			(
-				self.__handle_rx,
-				self.__handle_tx,
+				self.__handle_uart_rx,
+				self.__handle_uart_tx,
 			),
 		) = self.__ble.gatts_register_services(services)
+
+		self.__ble.gatts_set_buffer(self.__handle_uart_rx, 256, True)
+		self.__ble.gatts_set_buffer(self.__handle_uart_tx, 256, True)
 
 		printf('Services Registered')
 
 		if False:
-			printf('- rx:', self.__handle_rx)
-			printf('- tx:', self.__handle_tx)
+			printf('- rx:', self.__handle_uart_rx)
+			printf('- tx:', self.__handle_uart_tx)
 
 	def __advertise(self, adv_payload: bytes = None, resp_payload: bytes = None, interval_us: int = 100000):
 		self.__ble.gap_advertise(None)
@@ -80,14 +75,15 @@ class BLEUART(object):
 
 	def __irq_callback(self, event, data):
 		if event == IRQ.CENTRAL_CONNECT:
-			conn_handle, _, addr, = data # _: addr_type
+			conn_handle, _, addr, = data
 
 			self.__conn_handles.add(conn_handle)
 			self.__ble.gap_advertise(None)
 
 			printf(f'[{BLETools.decode_mac(addr)}] Connected [Handle: {conn_handle}]')
+
 		elif event == IRQ.CENTRAL_DISCONNECT:
-			conn_handle, _, addr, = data # _: addr_type
+			conn_handle, _, addr, = data
 
 			if conn_handle in self.__conn_handles:
 				self.__conn_handles.remove(conn_handle)
@@ -95,35 +91,57 @@ class BLEUART(object):
 			printf(f'[{BLETools.decode_mac(addr)}] Disconnected [Handle: {conn_handle}]')
 
 			self.__advertise()
+
 		elif event == IRQ.GATTS_WRITE:
 			conn_handle, attr_handle = data
 
-			# printf(f'GATTS Write [Handle: {conn_handle}, Attr_Handle: {attr_handle}]')
+			if conn_handle in self.__conn_handles and attr_handle == self.__handle_uart_rx:
+				received_data = bytes(self.__ble.gatts_read(self.__handle_uart_rx))
+				self.__rx_buffer += received_data
 
-			if attr_handle == self.__handle_rx:
 				if self.__rx_received_cb:
-					self.__rx_received_cb(bytes(self.__read(self.__handle_rx)))
+					self.__rx_received_cb(received_data)
+
 		elif event == IRQ.CONNECTION_UPDATE:
 			conn_handle, interval, latency, supervision_timeout, status = data
-
 			printf(f'Connection Update [Handle: {conn_handle}, Interval: {interval}, Latency: {latency}, Supervision_Timeout: {supervision_timeout}, Status: {status}]')
+
 		elif event == IRQ.GATTC_INDICATE:
 			conn_handle, value_handle, data = data
-
 			printf(f'GATTC Indicate [Handle: {conn_handle}, Value_Handle: {value_handle}, Data: {bytes(data)}]')
+
 		elif event == IRQ.SET_SECRET:
 			return False
+
 		elif event == IRQ.GET_SECRET:
 			return None
+
+		elif event == IRQ.MTU_EXCHANGED:
+			conn_handle, mtu = data
+			printf(f'MTU Exchanged [Handle: {conn_handle}, MTU: {mtu}]')
+
 		else:
 			printf(f'Uncaught IRQ Event: {event}, Data: {data}')
 
-	def send(self, data: bytes):
-		'''将数据写入本地缓存，并推送到中心设备'''
-		if self.__conn_handles is None:
-			return
+	def any(self):
+		return len(self.__rx_buffer)
 
-		self.__write(self.__handle_tx, data)
+	def read(self, count: int = None):
+		count = count or len(self.__rx_buffer)
+		result = self.__rx_buffer[0:count]
+		self.__rx_buffer = self.__rx_buffer[count:]
+
+		return result
+
+	def write(self, data: bytes):
+		'''将数据写入本地缓存，并推送到中心设备'''
+		self.__ble.gatts_write(self.__handle_uart_tx, data)
 
 		for conn_handle in self.__conn_handles:
-			self.__notify(conn_handle, self.__handle_tx, data)
+			self.__ble.gatts_notify(conn_handle, self.__handle_uart_tx, data)
+
+	def close(self):
+		for conn_handle in self.__conn_handles:
+			self.__ble.gap_disconnect(conn_handle)
+
+		self.__conn_handles.clear()
--- testing/test_uart.py
+++ testing/test_uart.py
@@ -3,11 +3,11 @@ Copyright © 2024 Walkline Wang (https://walkline.wang)
 Gitee: https://gitee.com/walkline/micropython-new-ble-library
 """
 import time
+from testing.utils.utilities import Utilities
 
 
-MODE_UART      = 0
-MODE_BLECONFIG = 1
-
+MODE_BLE_UART   = 0
+MODE_BLE_CONFIG = 1
 
 def run_bleuart_test():
 	from devices.uart.bleuart import BLEUART
@@ -15,7 +15,7 @@ def run_bleuart_test():
 	def rx_received_cb(data: bytes):
 		echo = bytes(reversed(data))
 		print(f'received data: {data}, sending back: {echo}')
-		bleuart.send(echo)
+		bleuart.write(echo)
 
 	bleuart = BLEUART(rx_received_cb=rx_received_cb)
 
@@ -32,34 +32,6 @@ def run_bleconfig_test():
 
 	print(f'ssid: {bleconfig.ssid}, password: {bleconfig.password}')
 
-def choose_an_option(title, options):
-	print(f'\n{title}:')
-
-	for index, option in enumerate(options, start=1):
-		if index == 1:
-			print(f'\x1b[32m  [{index}] {option}\033[0m')
-		else:
-			print(f'  [{index}] {option}')
-
-	selected = None
-
-	while True:
-		try:
-			selected = input('Choose an option: ')
-
-			if selected == '':
-				return 0
-
-			selected = int(selected)
-
-			assert type(selected) is int and 0 < selected <= len(options)
-
-			return selected - 1
-		except KeyboardInterrupt:
-			return
-		except:
-			pass
-
 
 if __name__ == '__main__':
 	options = [
@@ -67,10 +39,10 @@ if __name__ == '__main__':
 		'BLE Config, details: https://gitee.com/walkline/micropython_ble_config',
 	]
 
-	mode = choose_an_option('UART Test Mode', options)
+	mode = Utilities.choose_an_option('UART Test Mode', options)
 
 	if mode is not None:
-		if mode == MODE_UART:
+		if mode == MODE_BLE_UART:
 			run_bleuart_test()
 		else:
 			run_bleconfig_test()
